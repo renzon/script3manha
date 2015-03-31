1d1e1f519b8358da1e624907b7aa3723377790c1
renzon <renzon@gmail.com>
2015-03-31 16:19:20 -0300

    Primeira parte do crud de categoria funcionando

---
+++ backend/appengine/categoria/categoria_model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
+
+
+class Categoria(ndb.Model):
+    nome=ndb.StringProperty(required=True)
+
+    @classmethod
+    def query_ordenada_por_nome(cls):
+        return cls.query().order(Categoria.nome)
---
+++ backend/appengine/routes/categorias/edit.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from categoria.categoria_model import Categoria
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
+from routes import categorias
+from tekton.gae.middleware.redirect import RedirectResponse
+from tekton.router import to_path
+
+
+@no_csrf
+def index(categoria_id):
+    categoria=Categoria.get_by_id(int(categoria_id))
+    ctx={'categoria':categoria,
+         'salvar_path': to_path(atualizar)}
+    return TemplateResponse(ctx,'categorias/categorias_form.html')
+
+def atualizar(categoria_id,nome):
+    categoria=Categoria.get_by_id(int(categoria_id))
+    categoria.nome=nome
+    categoria.put()
+    return RedirectResponse(categorias)

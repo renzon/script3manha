7154816fb802cd9ccb9b0301965a6049a4c5629c
renzon <renzon@gmail.com>
2015-04-01 13:46:43 -0300

    Crud com form

--- backend/appengine/categoria/categoria_model.py
+++ backend/appengine/categoria/categoria_model.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from google.appengine.ext import ndb
+from google.appengine.ext.db import IntegerProperty
+from gaeforms.base import Form, StringField, IntegerField
+from gaeforms.ndb.form import ModelForm
 
 
 class Categoria(ndb.Model):
     nome=ndb.StringProperty(required=True)
     quantidade=  ndb.IntegerProperty(required=True)
+    criacao = ndb.DateTimeProperty(auto_now_add=True)
 
     @classmethod
     def query_ordenada_por_nome(cls):
         return cls.query().order(Categoria.nome)
+
+class CategoriaForm(ModelForm):
+    _model_class = Categoria
+    _include = [Categoria.nome]
+    quantidade=IntegerField(required=True,lower=0)
--- backend/appengine/routes/categorias/new.py
+++ backend/appengine/routes/categorias/new.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from categoria.categoria_model import Categoria
+from categoria.categoria_model import Categoria, CategoriaForm
 from config.template_middleware import TemplateResponse
 from routes import categorias
 from tekton.gae.middleware.redirect import RedirectResponse
 
 
-def salvar(nome, quantidade):
-    erros={}
-    numero = 0
-    try:
-        numero = int(quantidade)
-    except:
-        erros['quantidade']='Deve ser um número inteiro'
-    if nome =='':
-        erros['nome']='Campo Obrigatório'
+def salvar(**kwargs):
+    form= CategoriaForm(**kwargs)
+    erros=form.validate()
     if not erros:
-        categoria = Categoria(nome=nome, quantidade=numero)
+        propriedades=form.normalize()
+        categoria = Categoria(**propriedades)
         categoria.put()
         return RedirectResponse(categorias)
     else:
-        categoria = {'nome': nome, 'quantidade': quantidade}
-        ctx = {'categoria': categoria, 'erros':erros}
+        ctx = {'categoria': kwargs, 'erros':erros}
         return TemplateResponse(ctx, 'categorias/categorias_form.html')

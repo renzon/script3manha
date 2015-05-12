29c01cdb1abc50f8fb69a9e40c3e6a47d641cf9f
renzon <renzon@gmail.com>
2015-05-12 10:16:19 -0300

    Deleção dinâmica de produtos

--- backend/appengine/routes/produtos/rest.py
+++ backend/appengine/routes/produtos/rest.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
 from categoria.categoria_model import ProdutoForm, Produto
 from distutils import log
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_required, login_not_required
 from tekton.gae.middleware.json_middleware import JsonUnsecureResponse
 
+
+@login_not_required
+@no_csrf
+def deletar(produto_id):
+    key=ndb.Key(Produto,int(produto_id))
+    key.delete()
 
 @login_not_required
 @no_csrf
--- backend/appengine/static/js/produtos.js
+++ backend/appengine/static/js/produtos.js
@@ -6,14 +6,21 @@ $(document).ready(function () {
   var $categoriaInput = $('#categoria-input');
   var $produtosUl = $('#produtos-ul');
 
-  function adicionarProduto(produto){
-    var li='<li><button class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i></button>';
-    li=li+produto.nome +' - '+produto.preco +'</li>';
+  function adicionarProduto(produto) {
+    var li = '<li id="li-' + produto.id + '"';
+    li = li + '><button id="btn-deletar-' + produto.id + '"';
+    li = li + ' class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i></button>';
+    li = li + produto.nome + ' - ' + produto.preco + '</li>';
     $produtosUl.append(li);
+    $('#btn-deletar-' + produto.id).click(function () {
+      $.post('/produtos/rest/deletar', {produto_id: produto.id}, function () {
+        $("#li-"+produto.id).remove();
+      });
+    });
   }
 
-  $.get('/produtos/rest/listar',function(produtos){
-    $.each(produtos, function(index, p){
+  $.get('/produtos/rest/listar', function (produtos) {
+    $.each(produtos, function (index, p) {
       adicionarProduto(p);
     });
   });
@@ -32,18 +39,18 @@ $(document).ready(function () {
     $('.has-error').removeClass('has-error');
     $('.help-block').empty();
     $ajaxSaveGif.fadeIn();
-    $salvarBotao.attr('disabled','disabled')
+    $salvarBotao.attr('disabled', 'disabled')
     $.post('/produtos/rest/salvar',
         obterInputsDeProduto(),
         function (produto) {
           $('input[type="text"]').val('');
           adicionarProduto(produto);
-        }).error(function(erro){
-          for (propriedade in erro.responseJSON){
-            $('#'+propriedade+'-div').addClass('has-error');
-            $('#'+propriedade+'-span').text(erro.responseJSON[propriedade]);
+        }).error(function (erro) {
+          for (propriedade in erro.responseJSON) {
+            $('#' + propriedade + '-div').addClass('has-error');
+            $('#' + propriedade + '-span').text(erro.responseJSON[propriedade]);
           }
-        }).always(function(){
+        }).always(function () {
           $ajaxSaveGif.hide();
           $salvarBotao.removeAttr('disabled')
         });
